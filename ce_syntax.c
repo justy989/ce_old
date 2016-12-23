76cffdf64dd37c757a9ae1178c17eeb5492119b4
Justin Tardiff <justint989@gmail.com>
2016-12-23 13:50:22 -0500

    ugh again

--- ce_syntax.c
+++ ce_syntax.c
@@ -373,7 +373,7 @@ static int64_t syntax_is_c_typename(const char* line, int64_t start_offset)
      itr = line + start_offset;
      if(count >= 2 && itr[count-2] == '_' && itr[count-1] == 't') return count;
 
-#if 1
+#if 0
      // NOTE: Justin uses this while working on Bryte!
      if(isupper(*itr)){
           return count;
